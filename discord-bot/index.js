c6ddab89813f060d374ea8e8382d93b8d7ccc4d8
her_username <hank@Hanks-MacBook-Pro.local>
2023-03-20 14:25:16 -0400

    sdk comments

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -7,8 +7,16 @@ const {
 } = require("@hellomoon/api");
 const { paginateUntilEmpty } = require("../util/paginationToken.js");
 
+/* 
+    1. create a .env file 
+    2. require("dotenv").config(); to allow process.env to read the .env file
+    3. create a key, value pair in the file HELLOMOON_API_KEY="API_KEY_HERE"
+    4. checkout https://www.hellomoon.io/dashboard, to get your api key
+*/
 const helloMoonClient = new RestClient(process.env.HELLOMOON_API_KEY);
 
+// https://discordjs.guide/popular-topics/intents.html#privileged-intents
+// https://discord.com/developers/docs/topics/gateway#list-of-intents - checkout all of discord intent
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -22,6 +30,24 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+/* 
+    1. "interactionCreate" argument references the commands registered in 'registerCommands.js'
+    2. for example, 
+        ```
+            switch (interaction.commandName) 
+                case "ping": 
+        ```
+        references
+        ```
+            const commands = [
+            {
+                name: "ping",
+                description: "Replies with pong!",
+            },
+        ]
+        ```
+*/
+
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
@@ -30,6 +56,7 @@ client.on("interactionCreate", async (interaction) => {
       interaction.reply("Pong!");
       break;
     }
+    // https://docs.hellomoon.io/reference/post_v0-nft-collection-name
     case "get-collection-name": {
       const { data: collectionNameResponse } = await helloMoonClient.send(
         new CollectionNameRequest({
@@ -45,6 +72,7 @@ client.on("interactionCreate", async (interaction) => {
       });
       break;
     }
+    // https://docs.hellomoon.io/reference/post_v0-nft-social
     case "social-media-links": {
       const { data: nftSocialResponse } = await helloMoonClient.send(
         new NftSocialRequest({
@@ -74,11 +102,12 @@ client.on("interactionCreate", async (interaction) => {
       });
       break;
     }
+    // https://docs.hellomoon.io/reference/post_v0-nft-collection-name
+    // learn how to use paginationTokens
     case "get-all-collection_ids": {
       const limit = 1000;
 
       const collectionNamePromise = async (paginationToken) => {
-        console.log({ paginationToken });
         if (paginationToken) {
           return await helloMoonClient.send(
             new CollectionNameRequest({ limit, paginationToken })
@@ -96,7 +125,6 @@ client.on("interactionCreate", async (interaction) => {
       });
 
       const allCollections = await paginateUntilEmpty(collectionNamePromise);
-      console.log({ lengthOfCollections: allCollections.length });
 
       interaction.editReply({
         content: "finished getting all collection ids",
@@ -114,4 +142,5 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
+// bot logs in everytime to make a request
 client.login(process.env.DISCORD_KEY);
